a4016800d9f540f15724502b7cfa77ae1a97772e
Tyler <tylerbarrett2010+github@gmail.com>
2024-05-26 11:59:52 -0400

    Initial ui development

--- app/src/main/java/com/example/aviationassistant/FullscreenActivity.java
+++ app/src/main/java/com/example/aviationassistant/FullscreenActivity.java
@@ -4,7 +4,11 @@ import android.annotation.SuppressLint;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,6 +18,19 @@ import android.view.View;
 import android.view.WindowInsets;
 
 import com.example.aviationassistant.databinding.ActivityFullscreenBinding;
+
+/*
+Potential Widgets:
+- lat / long / dop
+- msl altitude
+- closest airport
+  - and distance
+  - metar / taf
+  - airspace restrictions based on altitude
+- voice memos
+- local sectional chart of airspace restrictions
+- pre-flight checklist for birds
+ */
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -32,32 +49,33 @@ public class FullscreenActivity extends AppCompatActivity {
      */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
+    private GpsPositionUpdater gpsPositionUpdater;
+
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler(Looper.myLooper());
-    private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
             // Delayed removal of status and navigation bar
-            if (Build.VERSION.SDK_INT >= 30) {
-                mContentView.getWindowInsetsController().hide(
-                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-            } else {
-                // Note that some of these constants are new as of API 16 (Jelly Bean)
-                // and API 19 (KitKat). It is safe to use them, as they are inlined
-                // at compile-time and do nothing on earlier devices.
-                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            }
+//            if (Build.VERSION.SDK_INT >= 30) {
+//                mContentView.getWindowInsetsController().hide(
+//                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+//            } else {
+//                // Note that some of these constants are new as of API 16 (Jelly Bean)
+//                // and API 19 (KitKat). It is safe to use them, as they are inlined
+//                // at compile-time and do nothing on earlier devices.
+//                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//            }
         }
     };
     private View mControlsView;
@@ -113,20 +131,34 @@ public class FullscreenActivity extends AppCompatActivity {
 
         mVisible = true;
         mControlsView = binding.fullscreenContentControls;
-        mContentView = binding.fullscreenContent;
+//        mContentView = findViewById(R.layout.activity_fullscreen);
 
         // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
+//        mContentView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                toggle();
+//            }
+//        });
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         binding.dummyButton.setOnTouchListener(mDelayHideTouchListener);
+
+        LocationManager locationmanager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationmanager.requestLocationUpdates(LocationManager.FUSED_PROVIDER, 5000, 10, gpsPositionUpdater);
     }
 
     @Override
@@ -163,13 +195,13 @@ public class FullscreenActivity extends AppCompatActivity {
 
     private void show() {
         // Show the system bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            mContentView.getWindowInsetsController().show(
-                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-        } else {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        }
+//        if (Build.VERSION.SDK_INT >= 30) {
+//            mContentView.getWindowInsetsController().show(
+//                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+//        } else {
+//            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+//        }
         mVisible = true;
 
         // Schedule a runnable to display UI elements after a delay
---
+++ app/src/main/java/com/example/aviationassistant/GpsPositionUpdater.java
@@ -0,0 +1,50 @@
+package com.example.aviationassistant;
+
+import android.location.Location;
+import android.location.LocationListener;
+
+import java.util.Random;
+
+
+public class GpsPositionUpdater extends PeriodicComponent implements LocationListener {
+
+    private final static double MAX_LOCATION_NOISE = 0.0002;
+    private final static double MAX_ACCURACY_NOISE = 0.2;
+
+    private double latestLat = 0.0;
+    private double latestLon = 0.0;
+    private double latestAccuracy = 0;
+    private boolean receivedLocation = false;
+
+    private Random r = new Random();
+
+    public GpsPositionUpdater() {
+        this.setIterationPeriodMs(100);
+    }
+
+    @Override
+    public Object update() {
+        if (receivedLocation) {
+
+            // TODO - move this artificial noise into the UI rendering code.
+            double noisyLat = latestLat + (r.nextDouble() * MAX_LOCATION_NOISE);
+            double noisyLon = latestLon + (r.nextDouble() * MAX_LOCATION_NOISE);
+            double noisyAccuracy = latestAccuracy + (r.nextDouble() * MAX_ACCURACY_NOISE);
+
+            return new GpsPosition(noisyLat, noisyLon, noisyAccuracy);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+
+        latestLat = loc.getLatitude();
+        latestLon = loc.getLongitude();
+        latestAccuracy = loc.getAccuracy();
+
+        receivedLocation = true;
+    }
+
+}
